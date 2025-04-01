9a4c6abfd1fc6760bf3e75d873b89666b636f136
ASDT000 <asdinushkatharidu@gmail.com>
2025-04-01 09:57:34 +0530

    feat: add cash payment and change calculation system
    - Implement cash input from customer
    - Add change amount calculation
    - Include denomination breakdown logic
    - Display payment summary in receipt

--- Source Codes/BillPrint.java
+++ Source Codes/BillPrint.java
@@ -35,7 +35,7 @@ class BillPrint{
 		int flour=input.nextInt();
 		System.out.print("\nSoap     Qty     - ");
 		int soap=input.nextInt();
-		
+		System.out.println();
 		final double basmathiPrice =250;
 		final double dhalPrice = 180;
 		final double sugerPrice = 150;
@@ -94,10 +94,108 @@ class BillPrint{
 		System.out.println("+                               +---------------+---------------+");
 		System.out.println("|                               |  Price        |       "+price+"  |");
 		System.out.println("+-----------------+-------------+---------------+---------------+");
-		System.out.println();
-		System.out.println();
-		System.out.println("Copyright (c) 2024 AS DINUSHKA THARIDU");
 		
+		System.out.print("\n\nEnter customer given amount : ");
+		double cash = input.nextDouble();
+		
+		int change = (int)(cash - price) ;
+		
+		System.out.println("\n+-----------------+-------------------+");
+		System.out.println("|    Net Amount   |   "+price+"          |");
+		System.out.println("+-----------------+-------------------+");
+		System.out.println("|    Cash         |   "+cash+"         |");
+		System.out.println("+-----------------+-------------------+");
+		System.out.println("|    Change       |   "+change+"            |");
+		System.out.println("+-----------------+-------------------+");
+		
+		int note5000 =0;
+		int note2000 =0;
+		int note1000 =0 ;
+		int note500  =0;
+		int note100  =0;
+		int note50   =0;
+		int note20   = 0;
+		int note10   = 0;
+		int note5   = 0;
+		int note2   = 0;
+		int note1   = 0;
+		
+		int no5000 =0;
+		int no2000 =0;
+		int no1000 =0 ;
+		int no500  =0;
+		int no100  =0;
+		int no50   =0;
+		int no20   = 0;
+		int no10   = 0;
+		int no5   = 0;
+		int no2   = 0;
+		int no1   = 0;
+		
+		note5000 = change / 5000;
+		no5000 = change%5000;
+		
+		note2000 = no5000/2000;
+		no2000 = change%2000;
+		
+		note1000 = no2000/1000;
+		no1000 = change%1000;
+		
+		note500 = no1000/500;
+		no500 = change%500;
+		
+		note100 = no500/100;
+		no100 = change%100;
+		
+		note50 = no100/50;
+		no50 = change%50;
+		
+		note20 = no50/20;
+		no20 = change%20;
+		
+		note10 = no20/10;
+		no10 = change%10;
+		
+		note5 = no10/5;
+		no5 = change%5;
+		
+		note2 = no5/2;
+		no2 = change%2;
+		
+		note1 = no2/1;
+		
+		int notes = note5000+note2000+note1000+note500+note100+note50+note20;
+		int coins = note10+note5+note2+note1;
+		
+		System.out.println("+\n-----------------+-------------+");
+		System.out.println("|      Value      |      No     |");
+		System.out.println("+-----------------+-------------+");
+		System.out.println("|     Rs.5000     |     "+note5000+"       |");
+		System.out.println("|-----------------|-------------|");
+		System.out.println("|     Rs.2000     |     "+note2000+"       |");
+		System.out.println("|-----------------|-------------|");
+		System.out.println("|     Rs.1000     |     "+note1000+"       |");
+		System.out.println("|-----------------|-------------|");
+		System.out.println("|     Rs.500      |     "+note500+"       |");
+		System.out.println("|-----------------|-------------|");
+		System.out.println("|     Rs.100      |     "+note100+"       |");
+		System.out.println("|-----------------|-------------|");
+		System.out.println("|     Rs.50       |     "+note50+"       |");
+		System.out.println("|-----------------|-------------|");
+		System.out.println("|     Rs.20       |     "+note20+"       |");
+		System.out.println("|-----------------|-------------|");
+		System.out.println("|     Rs.10       |     "+note10+"       |");
+		System.out.println("|-----------------|-------------|");
+		System.out.println("|     Rs.5        |     "+note5+"       |");
+		System.out.println("|-----------------|-------------|");
+		System.out.println("|     Rs.2        |     "+note2+"       |");
+		System.out.println("|-----------------|-------------|");
+		System.out.println("|     Rs.1        |     "+note1+"       |");
+		System.out.println("+-----------------|-------------+");
+		System.out.println("|   No Of Notes   |     "+notes+"       |");
+		System.out.println("+-----------------|-------------+");
+		System.out.println("|   No Of Coins   |     "+coins+"       |");
+		System.out.println("+-----------------|-------------+");
 		
 		
 	} 
